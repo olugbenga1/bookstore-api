98e79d6ecb48f7b3a190ba32bcce675f1bff4807
Olugbenga Odedele <odedeleg@gmail.com>
2022-05-26 02:58:35 -0500

    added order-books model method

--- src/models/order.ts
+++ src/models/order.ts
@@ -6,6 +6,13 @@ export type Order = {
   userId: string;
 };
 
+export type OrderBooks = {
+  id?: string;
+  quantity: number;
+  order_id: string;
+  book_id: string;
+};
+
 export class OrderStore {
   async index(): Promise<Order[]> {
     try {
@@ -50,6 +57,24 @@ export class OrderStore {
       const conn = await client.connect();
       const sql = "DELETE FROM orders WHERE id = ($1) RETURNING *";
       const res = await conn.query(sql, [id]);
+      conn.release();
+      return res.rows[0];
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+
+  async order_books(bookOrder: OrderBooks): Promise<OrderBooks> {
+    try {
+      const conn = await client.connect();
+      const sql =
+        "INSERT INTO order_books(quantity, order_id, book_id) VALUES ($1, $2, $3) RETURNING *";
+      const values = [
+        bookOrder.quantity,
+        bookOrder.order_id,
+        bookOrder.book_id,
+      ];
+      const res = await conn.query(sql, values);
       conn.release();
       return res.rows[0];
     } catch (error) {
